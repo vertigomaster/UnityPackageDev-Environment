108998503a3038050e9333501d544812de0322a6
vertigomaster <janoel777@gmail.com>
2025-05-04 23:52:36 -0500

    debugging, finally

---
+++ ProjectPewPew/Scripts/Shitty_InteractInputSystem.cs
@@ -0,0 +1,51 @@
+﻿using IDEK.Tools.GameplayEssentials.Interaction;
+using IDEK.Tools.Logging;
+using IDEK.Tools.ShocktroopExtensions;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace IDEK.Tools.Misc.DevEnv.ProjectPewPew
+{
+    public class Shitty_InteractInputSystem : MonoBehaviour
+    {
+        public InputActionReference interactAction;
+        
+        public InteractionAgent agent;
+
+        private void OnValidate()
+        {
+            gameObject.TryGetComponentIfNull(ref agent);
+        }
+
+        // private void OnEnable()
+        // {
+        //     interactAction.action.performed += OnDidInteractAction;
+        // }
+        //
+        // private void OnDisable()
+        // {
+        //     interactAction.action.performed -= OnDidInteractAction;
+        // }
+
+        private void Update()
+        {
+            if (interactAction.action.WasPerformedThisFrame())
+            {
+                ConsoleLog.Log("fooble - pressed interact action");
+                agent.TryTriggerNearestInteractable();
+            }
+        }
+
+        // private void OnDidInteractAction(InputAction.CallbackContext obj)
+        // {
+        //     ConsoleLog.Log("fooble OnDidInteractAction thing");
+        //     if (obj.ReadValue<bool>())
+        //     {
+        //         ConsoleLog.Log("fooble OnDidInteractAction and thing TRUE");
+        //
+        //         //do thing
+        //         agent.TryTriggerNearestInteractable();
+        //     }
+        // }
+    }
+}
