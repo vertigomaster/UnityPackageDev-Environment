fc5c622be81894c012f38e44eed5fafa56eb1d94
vertigomaster <janoel777@gmail.com>
2025-05-08 23:16:42 -0500

    some cleanup and also particle system for the pickup

---
+++ ProjectPewPew/Scripts/BasicPlayerInteractDriver.cs
@@ -0,0 +1,25 @@
+﻿using IDEK.Tools.Logging;
+using IDEK.Tools.ShocktroopExtensions;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace IDEK.Tools.GameplayEssentials.Interaction.Samples.Unity
+{
+    //Sample showing how to use the InputSystem to drive a GameplayEssentials Interaction agent.
+    public class BasicPlayerInteractDriver : MonoBehaviour
+    {
+        public InputActionReference interactAction;
+        public InteractionAgent agent;
+
+        private void OnValidate()
+        {
+            gameObject.TryGetComponentIfNull(ref agent);
+        }
+
+        private void Update()
+        {
+            if (!interactAction.action.WasPerformedThisFrame()) return;
+            agent.TryTriggerNearestInteractable();
+        }
+    }
+}
---
+++ ProjectPewPew/Scripts/EquippedToolDriver.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace IDEK.Tools.GameplayEssentials.Inventory.Samples.Unity
+{
+    /// <summary>
+    /// Sample how to use the inventory in tandem with equippables 
+    /// </summary>
+    public class EquippedToolDriver : MonoBehaviour
+    {
+        
+    }
+}
