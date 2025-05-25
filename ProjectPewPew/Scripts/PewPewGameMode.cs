951b0cc13cbc804d91c2a7e6139bcc6468f40ed7
vertigomaster <janoel777@gmail.com>
2025-05-24 23:51:42 -0500

    PewPewGameMode, perhaps inadvisably, though it doesn't matter too much since it's a game specific override, can return a player list by coupling it to the RuntimeCharacterRegistry

--- ProjectPewPew/Scripts/PewPewGameMode.cs
+++ ProjectPewPew/Scripts/PewPewGameMode.cs
@@ -1,10 +1,21 @@
-﻿using IDEK.Tools.GameplayEssentials.Core;
+﻿using System.Collections.Generic;
+using IDEK.Tools.GameplayEssentials.Characters.Unity;
+using IDEK.Tools.GameplayEssentials.Core;
 using UnityEngine;
 
 namespace IDEK.Tools.GameplayEssentials.Samples.PewPew
 {
-    public class PewPew_GameMode : GameMode
+    public class PewPewGameMode : GameMode
     {
-        
+        #region Overrides of GameMode
+
+        /// <inheritdoc />
+        public override IEnumerable<IPlayerCharacter> GetAllPlayers()
+        {
+            var x = RuntimeCharacterRegistry.Singleton.GetAllStates();
+            return x;
+        }
+
+        #endregion
     }
 }
