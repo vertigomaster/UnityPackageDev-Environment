09aaedef97cb68fb682bb008e9c74cdd01adb616
vertigomaster <janoel777@gmail.com>
2025-05-24 19:25:08 -0500

    created PewPewGameMode.cs

---
+++ ProjectPewPew/Scripts/PewPewGameMode.cs
@@ -0,0 +1,10 @@
+﻿using IDEK.Tools.GameplayEssentials.Core;
+using UnityEngine;
+
+namespace IDEK.Tools.GameplayEssentials.Samples.PewPew
+{
+    public class PewPew_GameMode : GameMode
+    {
+        
+    }
+}
