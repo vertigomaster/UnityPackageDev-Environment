c9b5bd43a04c23ac31658181d273d39624bc428b
vertigomaster <janoel777@gmail.com>
2025-05-27 00:56:34 -0500

    PewPew enemy reorg plus entering data and changes from the packages

---
+++ ProjectPewPew/Scripts/Behaviour/SetAllRenderersToMaterial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace IDEK.Tools.GameplayEssentials.Samples.PewPew
+{
+    public class SetAllRenderersToMaterial : MonoBehaviour
+    {
+#if ODIN_INSPECTOR
+        [Sirenix.OdinInspector.Button]
+#endif
+        public void Activate(Material material)
+        {
+            ListPool<Renderer>.Get(out List<Renderer> rendererList);
+            
+            GetComponentsInChildren(rendererList);
+            foreach (var renderer in rendererList)
+            {
+                renderer.material = material;
+            }
+            
+            ListPool<Renderer>.Release(rendererList);
+        }
+    }
+}
--- ProjectPewPew/Scripts/Core/PewPewGameMode.cs
+++ ProjectPewPew/Scripts/Core/PewPewGameMode.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections.Generic;
 using IDEK.Tools.GameplayEssentials.Characters.Unity;
 using IDEK.Tools.GameplayEssentials.Core;
+using IDEK.Tools.Logging;
 using UnityEngine;
 
 namespace IDEK.Tools.GameplayEssentials.Samples.PewPew
 {
     public class PewPewGameMode : GameMode
     {
+        protected virtual void Start()
+        {
+            ConsoleLog.LogWarning("TODO: Need to implement player leveling so that " +
+                "GetPlayerEncounterLevel() can be properly filled out");
+        }
+
         #region Overrides of GameMode
 
         /// <inheritdoc />
@@ -14,6 +21,13 @@ namespace IDEK.Tools.GameplayEssentials.Samples.PewPew
         {
             var x = RuntimeCharacterRegistry.Singleton.GetAllStates();
             return x;
+        }
+
+        /// <inheritdoc />
+        public override int GetPlayerEncounterLevel()
+        {
+            //TODO: implement player leveling so that we can fill this out properly
+            return 1;
         }
 
         #endregion
