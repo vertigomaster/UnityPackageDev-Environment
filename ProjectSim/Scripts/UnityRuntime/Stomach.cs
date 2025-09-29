8e519bb133337b2017be87924649aa61164fce2c
vertigomaster <janoel777@gmail.com>
2025-09-28 20:53:53 -0500

    a little bit of progress on ProjectSim - did more but it was an overengineering blackhole, so I ended up reverting it

--- ProjectSim/Scripts/UnityRuntime/Stomach.cs
+++ ProjectSim/Scripts/UnityRuntime/Stomach.cs
@@ -3,6 +3,7 @@ using IDEK.Tools.GameplayEssentials.Inventory;
 using IDEK.Tools.GameplayEssentials.Inventory.Unity;
 using IDEK.Tools.GameplayEssentials.Items;
 using IDEK.Tools.GameplayEssentials.Quantities;
+using IDEK.Tools.ShocktroopExtensions;
 using UnityEngine;
 
 namespace IDEK.ProjectSim.Unity.Runtime
@@ -11,11 +12,19 @@ namespace IDEK.ProjectSim.Unity.Runtime
     /// Represents the eaten items within a creature's stomach.
     /// Useful for making certain effects happen as a result of what has been eaten 
     /// </summary>
-    public class StomachInventory : InventoryComponent<FiniteVolumeInventoryRuntime>
+    [RequireComponent(typeof(InventoryComponent))]
+    public class Stomach : MonoBehaviour
     {
+        public InventoryComponent inventory;
+
+        private void OnValidate()
+        {
+            gameObject.TryGetComponentIfNull(ref inventory);
+        }
+
         public bool TryEat(InventoryItemRepresentation itemRep)
         {
-            return itemRep.TryObtain(Runtime);
+            return itemRep.TryObtain(inventory.Runtime);
         }
     }
 }
