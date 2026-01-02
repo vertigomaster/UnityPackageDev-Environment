455a5642e5cea7ed8e9977f164555ab8a745ef86
vertigomaster <janoel777@gmail.com>
2026-01-01 22:52:57 -0600

    Big mess attempting a gameplay ability system. Stable, but not ready for use; bigger undertaking than expected. Further work on this will be on its own branch, I promise.
    Also re-organized some files. Def should've committed those first. Would have been way less messy.

---
+++ ProjectPewPew/Scripts/Effects/OxygenDrainAttributeEffect.cs
@@ -0,0 +1,35 @@
+﻿using IDEK.Tools.GameplayEssentials.Effects;
+using IDEK.Tools.Logging;
+using UnityEngine;
+
+namespace IDEK.Tools.GameplayEssentials.Samples.PewPew.Effects
+{
+    // [CreateAssetMenu(fileName = "OxyChangeEffect", menuName = "Pew Pew/Effects/Oxygen Level Change", order = 100)]
+    public class OxygenDrainAttributeEffect : TickingGameplayAttributeEffect
+    {
+        public float drainStrength = 0.05f;
+        //when active
+        //every frame, make oxygen decrease by x * deltaTime
+
+        #region Overrides of TickingGameplayEffectAsset
+
+        /// <inheritdoc />
+        protected override void Tick(GameplayAttributeEffectSystem targetSystem, float deltaTime)
+        {
+            //look for an oxygen attribute
+            if (!targetSystem.TryGetAttribute<OxygenAttribute>(out var oxy)) return;
+            
+            //TODO; do something like IDamageable does to manage incoming buffs
+            oxy.BaseValue -= drainStrength * deltaTime;
+            ConsoleLog.Log("Oxygen at " + oxy.BaseValue + "%");
+        }
+
+        /// <inheritdoc />
+        public override object Clone()
+        {
+            return (OxygenDrainAttributeEffect)MemberwiseClone();
+        }
+
+        #endregion
+    }
+}
