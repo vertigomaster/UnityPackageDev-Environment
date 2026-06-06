6fc562e096ea1fe2882db9b2d8f3ed3451e2b815
vertigomaster <janoel777@gmail.com>
2026-06-05 20:19:48 -0500

    Rearranged file structure for climate code to make more sense, wrote prototype stub impl for IUnityClimateDataService as a monobehaviour.

---
+++ PlantGame/Scripts/Ecology/Unity/Climate/PrototypeClimateDataService.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace IDEK.PlantGame.Ecology.Unity
+{
+    public class PrototypeClimateDataService : MonoBehaviour, IUnityClimateDataService
+    {
+        [SerializeField]
+        private WeatherState constantWeatherState = new WeatherState();
+
+        #region Implementation of IUnityClimateDataService
+        
+        /// <inheritdoc />
+        public WeatherState GetWeatherAt(Vector3 worldPosition)
+        {
+            return constantWeatherState;
+        }
+
+        /// <inheritdoc />
+        public WeatherState GetAveragedWeatherAt(Vector3 worldPosition, float range)
+        {
+            return constantWeatherState;
+        }
+
+        #endregion
+    }
+}
